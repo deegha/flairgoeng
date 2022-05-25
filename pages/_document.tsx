0c7bb8a6ab4521b55519e9a8486483b55920c30f
Ravin Perera <bravinperera@gmail.com>
2022-05-25 21:59:15 +0530

    Added changes to processes section

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,18 +5,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="preload"
-            href="/fonts/FontsFree-Net-RocGrotesk.ttf"
-            as="font"
-            crossOrigin=""
-          />
+          <link rel="preload" href="/fonts/FontsFree-Net-RocGrotesk.ttf" as="font" crossOrigin="" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,500&display=swap"
             rel="stylesheet"
